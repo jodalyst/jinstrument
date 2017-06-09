745f570a6a012397e6f9d2147824e3a827c09e23
Joseph Steinmeyer <jodalyst@mit.edu>
2017-06-09 16:53:42 -0400

    more structure to alternator

--- js/alternator.js
+++ js/alternator.js
@@ -1,9 +1,33 @@
 
 
+$(document).on("change", ".alternator",function(){
+    var parent; 
+    var tempid = $(this).parent().attr("id");
+    var value = $(this).val();
+    if (value = 1.0){
+        $("#"+tempid+"_alternator").val
 
-function Alternator(control_id,command_id,period){
+$(window).load(function(event){
+
+    $('alternator').on('change',function(){
+        var value = $(this).val();
+        if (value == 1.0){
+             
+        }  
+    }
+
+
+}
+
+
+function Alternator(command_id,period,resolution){
     this.control_id = control_id;
     this.command_id = command_id;
     this.period = period;
-
+    this.resolution = resolution;
+    this.state = false; //false = not alternating, true = alternating
+    this.current_value = 0;
+    this.toggle(){
+        $("#"+this.command_id
+    
 }
