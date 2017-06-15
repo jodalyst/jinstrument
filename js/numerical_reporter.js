db5775b17fc3fb15bb6d8869df2db2b1c7767794
Joseph Steinmeyer <jodalyst@mit.edu>
2017-06-15 09:37:22 -0400

    final changes to numerical_reporter

--- js/numerical_reporter.js
+++ js/numerical_reporter.js
@@ -9,15 +9,14 @@ function Numerical_Reporter(div_id,title,range,color,bg_color){
     this.value = 0.0;
     this.unique = Math.floor(Math.random() * (100000));
     $("#"+this.div_id).append("<div class ='number_holder' id=\""+this.div_id+this.unique+"_holder\"></div>");
-    $("#"+this.div_id+this.unique+"_holder").append("<div class ='number_title' id=\""+this.div_id+"_title\">"+title+"</div>");
-    $("#"+this.div_id+this.unique+"_holder").append("<div class ='reported_number' style=\"color:"+ this.color+";background-color:"+this.bg_color+";\" id=\""+this.div_id+"_number\">"+this.value+"</div>");
-    this.step = function(value){
-        if (value> range[1]){
+    $("#"+this.div_id+this.unique+"_holder").append("<div class ='number_title' id=\""+this.div_id+this.unique+"_title\">"+title+"</div>");
+    $("#"+this.div_id+this.unique+"_holder").append("<div class ='reported_number' style=\"color:"+ this.color+";background-color:"+this.bg_color+";\" id=\""+this.div_id+this.unique+"_number\">"+this.value+"</div>");
+    this.step = function(value){ 
+        if (range[1] != null && value> range[1]){
             value = range[1];
-        }else if (value <range[0]){
+        }else if (range[0] != null && value <range[0]){
             value= range[0];
         }
-        console.log("Updating with " + String(value));
-        $("#"+this.div_id+"_number").html(value);
+        $("#"+this.div_id+this.unique+"_number").html(value);
     };
 };
