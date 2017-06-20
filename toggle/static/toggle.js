eb0d8854720b14ef09d831b14377c389905ac462
Joseph Steinmeyer <jodalyst@mit.edu>
2017-06-20 11:24:40 -0400

    fixed style rendering bug...must call create on div containing both label and input

--- toggle/static/toggle.js
+++ toggle/static/toggle.js
@@ -10,15 +10,15 @@ function Toggle(div_id,title,names,unique,socket=null){
         $("#"+div_id).append("<div class ='toggle_holder' id=\""+div_id+unique+"_holder\"></div>");
         $("#"+div_id+unique+"_holder").append("<label for =\"" + div_id+unique+"toggler"+"\">"+title+": </label>");
         $("#"+div_id+unique+"_holder").append("<select name=\""+ div_id+unique+"toggler" +"\" id=\""+div_id+unique+"toggle"+"\" data-role=\"slider\"><option value=\""+names[0]+"\">"+names[0]+"</option><option value=\""+names[1]+"\">"+names[1]+" </option></select>");
-        $("#"+div_id+unique+"_holder").append('<label for="slider-1">Input slider:</label><input type="range" name="slider-1" id="slider-1" value="60" min="0" max="100" />');
+        //$("#"+div_id+unique+"_holder").append('<label for="slider-1">Input slider:</label><input type="range" name="slider-1" id="slider-1" value="60" min="0" max="100" />');
         built = true;
-        $("#"+div_id+unique+"toggle").trigger("create");
+        $("#"+div_id+unique+"_holder").trigger("create");
         console.log("just enjanced");
         console.log("#"+div_id+unique+"toggle");
     }
     setup();
     if (socket != null){
-        socket.on("update_"+unique,function(va){console.log("hit");if (built){$('#'+div_id+unique+"toggle").val(va).trigger('refresh');}});
+        socket.on("update_"+unique,function(va){console.log("hit");if (built){$('#'+div_id+unique+"toggle").val(va).slider('refresh');}});
         $('#'+div_id+unique+"toggle").on('change',function(){
             socket.emit('reporting', {'unique':unique, 'data':$(this).val()});
         });
