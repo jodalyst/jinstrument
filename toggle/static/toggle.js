2ab6c3132d7b69c2c1518037ad26196ddb90098a
Joseph Steinmeyer <jodalyst@mit.edu>
2017-06-20 11:11:31 -0400

    fixed page load error handling bug regarding late creation. jquery-3.2 does not work with jquery-mobile most recent...keep in mind

--- toggle/static/toggle.js
+++ toggle/static/toggle.js
@@ -5,11 +5,20 @@ function Toggle(div_id,title,names,unique,socket=null){
     var value; //holds toggle value right now
     var unique = String(unique); //unique identifying number
     var socket = socket;
-    $("#"+div_id).append("<div class ='toggle_holder' id=\""+div_id+unique+"_holder\"></div>");
-    $("#"+div_id+unique+"_holder").append("<label for =\"" + div_id+unique+"toggler"+"\">"+title+": </label>");
-    $("#"+div_id+unique+"_holder").append("<select name=\""+ div_id+unique+"toggler" +"\" id=\""+div_id+unique+"toggle"+"\" data-role=\"slider\"><option value=\""+names[0]+"\">"+names[0]+"</option><option value=\""+names[1]+"\">"+names[1]+" </option></select>");
+    var built = false;
+    var setup = function(){
+        $("#"+div_id).append("<div class ='toggle_holder' id=\""+div_id+unique+"_holder\"></div>");
+        $("#"+div_id+unique+"_holder").append("<label for =\"" + div_id+unique+"toggler"+"\">"+title+": </label>");
+        $("#"+div_id+unique+"_holder").append("<select name=\""+ div_id+unique+"toggler" +"\" id=\""+div_id+unique+"toggle"+"\" data-role=\"slider\"><option value=\""+names[0]+"\">"+names[0]+"</option><option value=\""+names[1]+"\">"+names[1]+" </option></select>");
+        $("#"+div_id+unique+"_holder").append('<label for="slider-1">Input slider:</label><input type="range" name="slider-1" id="slider-1" value="60" min="0" max="100" />');
+        built = true;
+        $("#"+div_id+unique+"toggle").trigger("create");
+        console.log("just enjanced");
+        console.log("#"+div_id+unique+"toggle");
+    }
+    setup();
     if (socket != null){
-        socket.on("update_"+unique,function(va){$('#'+div_id+unique+"toggle").val(va).slider('refresh');});
+        socket.on("update_"+unique,function(va){console.log("hit");if (built){$('#'+div_id+unique+"toggle").val(va).trigger('refresh');}});
         $('#'+div_id+unique+"toggle").on('change',function(){
             socket.emit('reporting', {'unique':unique, 'data':$(this).val()});
         });
