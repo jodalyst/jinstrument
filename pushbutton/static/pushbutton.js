b46eb70ac5b4a1325b6f4fd90b36d213e96423de
Joseph Steinmeyer <jodalyst@mit.edu>
2017-06-20 19:35:31 -0400

    final edits on push button...mostly working

--- pushbutton/static/pushbutton.js
+++ pushbutton/static/pushbutton.js
@@ -7,18 +7,20 @@ function PushButton(div_id,label,color,bg_color,unique,socket=null){
     var unique = String(unique); //unique identifying number
     var socket = socket;
     var setup = function(){
-        $("#"+div_id).append("<div class ='toggle_holder' id=\""+div_id+unique+"_holder\"></div>");
+        $("#"+div_id).append("<div class ='button_holder' id=\""+div_id+unique+"_holder\"></div>");
         //$("#"+div_id+unique+"_holder").append("<button class=\"ui-button\" id=\""+ div_id+unique+"button" +"\" style=\"background-color:" + bg_color+";color: " + color +"\">"+label+"</button>");
         $("#"+div_id+unique+"_holder").append("<button class=\"ui-button\" id=\""+ div_id+unique+"button" +"\">"+label+"</button>");
         $("#"+div_id+unique+"_holder").trigger("create");
     }
     setup();
     if (socket != null){
-        $('#'+div_id+unique+"toggle").on('push',function(){
+        console.log('#'+div_id+unique+"button");
+        $('#'+div_id+unique+"button").on('click',function(){
             console.log("PUSH");
             socket.emit('reporting', {'unique':unique, 'data':"Push"});
         });
-        $('#'+div_id+unique+"toggle").off('push',function(){
+        //off(clicking not working...is fine for now')
+        $('#'+div_id+unique+"button").off('click',function(){
             console.log("UNPUSH");
             socket.emit('reporting', {'unique':unique, 'data':"Unpush"});
         });
